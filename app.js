6b81e1947d4d6c0882728ec0b7c2046495b848e8
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@dsn127m1246733.cfp127aluno.local>
2025-09-16 14:17:14 -0300

    Criando app

---
+++ app.js
@@ -0,0 +1,46 @@
+/*******************************************************************************************************************
+ * Objetivo ==> Api referente ao projeto de adoçao de pets
+ * Data ==> 20/05/2025
+ * Autor ==> Israel
+ * Versão ==> 1.0
+ * 
+ * Observação:
+ *      ****** PARA CONFIGURAR E INSTALAR A API, PRECISAMOS DAS SEGUINTE BIBLIOTECA ********
+ *                      express                  npm install express --save
+ *                      cors                     npm install cors --save
+ *                      body-parser              npm install body-parser --save
+ * 
+ *      ****** PARA CONFIGURAR E INSTALAR O ACESSO AO BANCO DE DADOS, PRECISAMOS: ********
+ *                      prisma                 npm install prisma --save ( Conexão com o BD )
+ *                      prisma/client          npm install @prisma/client --save ( Executa scripts no BD )
+ *    
+ *      ******* Após a instalação do prisma e do primsa client, devemos:
+ *                  npx prisma init ( ! INICIALIAR O PRISMA NO PROJETO !)
+ * 
+ *      ******* Para realizar o sincronismo do prisma com o BD, devemos executar o seguinte comando:
+ *                  npx prisma migrate devs
+ * 
+ * 
+ * ************************
+ * 
+ * POST E PUT PRECISAM DO BodyParserJson para funcionar
+ **********************************************************************************************************************/
+
+ const express   = require('express')
+ const cors      = require('cors')
+ const app       = express()
+
+ app.use(cors())
+ app.use(express.json())
+
+ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+ const routes = require('./routes/route.js')
+
+
+ // Prefixo base da API
+app.use('/v1/controle-pet', routes)
+
+app.listen('8080',function(){
+    console.log('API FUNCIONANDO AGUARDANDO REQUESIÇÕES CHEFE...')
+})
