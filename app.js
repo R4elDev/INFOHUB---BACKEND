535c7594a1676c3a5ee7fd9ebe68838b49c57400
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-09-18 10:28:25 -0300

    Feito a model de usuario

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
  *                  npx prisma init ( ! INICIALIAR O PRISMA NO PROJETO !)
  * 
  *      ******* Para realizar o sincronismo do prisma com o BD, devemos executar o seguinte comando:
- *                  npx prisma migrate devs
+ *                  npx prisma migrate dev
  * 
  * 
  * ************************
